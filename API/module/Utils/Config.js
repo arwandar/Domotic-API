14729a093746e2019d624011fd450034cea452c4
arwandar <arwandar@gmail.com>
2017-03-22 21:21:36 +0100

    préparation pour avoir plusieurs projets relatifs à domotic

---
+++ API/module/Utils/Config.js
@@ -0,0 +1,13 @@
+var yaml = require( 'js-yaml' );
+var path = require( 'path' );
+var fs = require( 'fs' );
+
+var Config = {
+    get: function( configPath ) {
+        var file = fs.readFileSync( path.join( __dirname, configPath ) );
+        var params = yaml.load( file );
+        return params;
+    }
+}
+
+module.exports = Config;
